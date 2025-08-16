53a0ca4537800d116b3564e1039552df10d79659
Antonio <mena120600@gmail.com>
2025-08-16 17:35:54 -0400

    Fixed but missing collection_function_entries

--- recompiler_tool/build/RecompilerTests[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test([=[AnalysisHelpers.IsBranchLikely]=]  C:/Users/Owner/Desktop/PS2_Recomp/Ps2Recomp/recompiler_tool/build/Debug/RecompilerTests.exe [==[--gtest_filter=AnalysisHelpers.IsBranchLikely]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[AnalysisHelpers.IsBranchLikely]=]  PROPERTIES WORKING_DIRECTORY C:/Users/Owner/Desktop/PS2_Recomp/Ps2Recomp/recompiler_tool/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set(  RecompilerTests_TESTS AnalysisHelpers.IsBranchLikely)
